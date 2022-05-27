7b69912093c8ab52b30fe44acaf5ed9e9247556d
Kiankalu Pedro <kpkia1@hotmail.com>
2022-05-27 13:21:40 +0100

    added a new text in the java file

---
+++ src/main/java/com/example/hellocd/HelloCdApplication.java
@@ -0,0 +1,22 @@
+package com.example.hellocd;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellocdApplication {
+
+
+	@RequestMapping("/")
+	String home() {
+		return "Welcome my pals!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellocdApplication.class, args);
+	}
+
+}
