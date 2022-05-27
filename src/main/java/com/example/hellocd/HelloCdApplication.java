ea64f323fb9045fd209aa83f14b62b9a2623f6d1
Kiankalu Pedro <kpkia1@hotmail.com>
2022-05-27 13:23:32 +0100

    Changed text to friends

--- src/main/java/com/example/hellocd/HelloCdApplication.java
+++ src/main/java/com/example/hellocd/HelloCdApplication.java
@@ -12,7 +12,7 @@ public class HellocdApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Welcome my pals!";
+		return "Welcome my friends!";
 	}
 
 	public static void main(String[] args) {
